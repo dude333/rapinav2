fd1ea5e345eb844ea2fc153588d136c5e7f3ea9f
Adriano Prado <dev@dude333.com>
2023-09-07 15:22:58 -0300

    Incluir dado anual caso caia no 4o trimestre

--- pkg/contabil/repositorio/repositorio_sqlite_queries.go
+++ pkg/contabil/repositorio/repositorio_sqlite_queries.go
@@ -71,10 +71,13 @@ func sqlTrimestral(ids []int, consolidado bool) string {
 	         THEN yearly_value - (q1_value + q2_value + q3_value)
 	         ELSE 0
 	    END AS q4_calculated,
-		CASE WHEN q1_value IS NOT NULL AND q2_value IS NOT NULL AND q3_value IS NOT NULL
-			THEN COALESCE(yearly_value, 0)
-			ELSE 0
-		END AS yearly_value
+        CASE WHEN q1_value IS NOT NULL AND q2_value IS NOT NULL AND q3_value IS NOT NULL
+            THEN COALESCE(yearly_value, 0)
+            ELSE CASE WHEN SUBSTR(data_fim_exerc, 6, 2) IN ('10', '11', '12')
+                THEN COALESCE(yearly_value, 0)
+                ELSE 0
+                END
+            END AS yearly_value
 	FROM (
 	    SELECT
 	        CASE WHEN c.data_ini_exerc <> '' THEN SUBSTR(c.data_ini_exerc, 1, 4)
@@ -95,7 +98,10 @@ func sqlTrimestral(ids []int, consolidado bool) string {
 		        WHEN meses =  3 AND SUBSTR(c.data_ini_exerc, 6, 2) IN ('07', '08', '09') THEN valor 
 		        WHEN meses = 12 AND SUBSTR(c.data_fim_exerc, 6, 2) IN ('07', '08', '09') THEN valor
 		        ELSE NULL END) AS q3_value,
-	        (CASE WHEN meses = 12 AND c.data_fim_exerc = MAX(c.data_fim_exerc) THEN valor ELSE NULL END) AS yearly_value
+            (CASE 
+                WHEN meses = 12 AND c.data_fim_exerc = MAX(c.data_fim_exerc) THEN valor 
+                ELSE NULL END) AS yearly_value
+
 	    FROM
 	        empresas e
 	    JOIN contas c ON e.id = c.id_empresa
