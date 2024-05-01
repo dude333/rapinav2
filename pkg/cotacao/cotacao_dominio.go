d1ada5ea62ea55659b57d98b22098cf96b1bb8ad
Adriano Prado <dev@dude333.com>
2024-05-01 00:19:17 -0300

    Adicionar métodos

--- pkg/cotacao/cotacao_dominio.go
+++ pkg/cotacao/cotacao_dominio.go
@@ -17,6 +17,39 @@ type Ativo struct {
 	Volume       float64
 }
 
+// PreçoTípico é a média aritmética entre o preço máximo, o preço mínimo e o
+// preço de fechamento
+func (a *Ativo) PreçoTípico() rapina.Dinheiro {
+	return rapina.Dinheiro{
+		Moeda:  a.Encerramento.Moeda,
+		Valor:  (a.Máxima.Valor + a.Mínima.Valor + a.Encerramento.Valor) / 3,
+		Escala: a.Encerramento.Escala,
+	}
+}
+
+// VWAP é o preço ponderado médio SUM(PRECO_TÍPICO * VOLUME) / SUM(VOLUME)
+func VWAP(ativos []*Ativo) rapina.Dinheiro {
+	if len(ativos) == 0 {
+		return rapina.Dinheiro{}
+	}
+	var totalPreçoTípico float64
+	var totalVolume float64
+	for _, ativo := range ativos {
+		pt := ativo.PreçoTípico()
+		totalPreçoTípico += pt.Valor * ativo.Volume
+		totalVolume += ativo.Volume
+	}
+	var v float64
+	if totalVolume > 0 {
+		v = totalPreçoTípico / totalVolume
+	}
+	return rapina.Dinheiro{
+		Moeda:  ativos[0].Encerramento.Moeda,
+		Valor:  v,
+		Escala: ativos[0].Encerramento.Escala,
+	}
+}
+
 // Ativos -------------------------------------------------
 type Ativos []Ativo
 
