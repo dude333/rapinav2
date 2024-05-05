42cbda48dee859d01d1a9e7d7bfd44cc90265d1f
Adriano Prado <dev@dude333.com>
2024-05-05 00:15:45 -0300

    Melhorar cmd cotacao

--- cmd/cotacao.go
+++ cmd/cotacao.go
@@ -1,27 +1,11 @@
-/*
-Copyright © 2024 Adriano Prado <prado@gmx.net>
+// SPDX-FileCopyrightText: 2024 Adriano Prado <dev@dude333.com>
+//
+// SPDX-License-Identifier: MIT
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package main
 
 import (
+	rapina "github.com/dude333/rapinav2"
 	"github.com/dude333/rapinav2/pkg/cotacao"
 	"github.com/dude333/rapinav2/pkg/progress"
 	"github.com/spf13/cobra"
@@ -46,17 +30,46 @@ func init() {
 	rootCmd.AddCommand(cotacaoCmd)
 }
 
+// report --> need quote --> get quote and VWAP
+// on report: quote.FromCNPJ("84.429.695/0001-11", "2019-01-01", -10)
+// 1. CNPJ -> código
+// 2. código -> cotações
+// 3. VWAP
 func cotação(_ *cobra.Command, args []string) {
-	c, err := cotacao.NovoServiço(db(), flags.tempDir)
-	if err != nil {
-		progress.Fatal(err)
-	}
-	ativos, err := c.Cotação(args[0], args[1])
-	if err != nil {
-		progress.Error(err)
-		return
-	}
-	for _, ativo := range ativos {
-		progress.Status("%+v", ativo)
+	for empresa := range menuEmpresas() {
+		// empresa := rapina.Empresa{
+		// 	CNPJ: "84.429.695/0001-11",
+		// 	Nome: "WEG S.A.",
+		// }
+		if empresa.CNPJ == "" {
+			continue
+			// return
+		}
+		progress.Status("empresa: %+v", empresa)
+
+		c, err := cotacao.NovoServiço(db(), flags.tempDir)
+		if err != nil {
+			progress.Fatal(err)
+		}
+
+		d, err := rapina.NovaData(args[0])
+		if err != nil {
+			d = rapina.DiaUtilAnterior(rapina.Hoje())
+		}
+
+		var all []*cotacao.Ativo
+		for n := 0; n < 10; n++ {
+			d = rapina.DiaUtilAnterior(d)
+			ativos, err := c.Cotação(empresa, d)
+			if err != nil {
+				progress.Error(err)
+				continue
+			}
+			all = append(all, ativos...)
+		}
+		for _, ativo := range all {
+			progress.Status("%+v", ativo)
+		}
+		progress.Status("VWAP: %+v", cotacao.VWAP(all))
 	}
 }
